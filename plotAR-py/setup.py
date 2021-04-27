d0972a1ce474ca225b99ef27f405635a123f240b
Philipp Thomann <pht@gmx.ch>
2021-04-27 21:29:44 +0200

    changed classifier to be compatible with pypi

--- plotAR-py/setup.py
+++ plotAR-py/setup.py
@@ -23,7 +23,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: AGPL 3',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
