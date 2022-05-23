56b1dbebd394cab5d1cba319e75a13ab37c818ce
Philipp Thomann <pht@gmx.ch>
2022-05-23 15:17:49 +0200

    refactored export function

--- plotAR-py/plotar/export/__init__.py
+++ plotAR-py/plotar/export/__init__.py
@@ -1,4 +1,39 @@
 
+from pathlib import Path
+import json
+
 from .usd import data2usd_ascii, data2usdz
 from .gltf import data2gltf
 from .common import data2obj
+
+
+def export(input, data_path, format_list, out=None, check=False):
+    data_path = Path(data_path)
+    out_name_generated = False
+    for format in format_list:
+        if out is None or out_name_generated:
+            out_name_generated = True
+            if data_path.name.endswith('.'):
+                data_path = data_path.parent / data_path.name[:-1]
+            out = data_path.with_suffix("."+format)
+        print(f"Generating format {format} in file {out}")
+        mode = 'wb' if format.startswith("usd") or format == 'glb' else 'w'
+        with open(out, mode) as outfile:
+            if format == 'gltf':
+                result = data2gltf(input).format_gltf()
+                json.dump(result, outfile)
+            elif format == 'glb':
+                result = data2gltf(input).format_glb()
+                outfile.write(result)
+            elif format == 'obj':
+                result = data2obj(input)
+                outfile.write(result)
+            elif format == 'usda':
+                usda, assets = data2usd_ascii(input)
+                outfile.write(usda.encode("utf-8"))
+                for key, val in assets.items():
+                    with open(key, 'wb') as f:
+                        f.write(val)
+            else:
+                result = data2usdz(input, save_usda=False, check=check)
+                outfile.write(result)
--- plotAR-py/plotar/export/__main__.py
+++ plotAR-py/plotar/export/__main__.py
@@ -3,9 +3,7 @@ from pathlib import Path
 
 import click
 
-from .common import data2obj
-from .usd import data2usdz, data2usd_ascii
-from .gltf import data2gltf
+from . import export
 
 if False:
     print("Welcome")
@@ -26,7 +24,7 @@ if False:
 @click.argument('out', default='', type=click.Path())
 @click.option('-f', '--format', default=None, help="format: gltf glb usdz usda obj. Can be a comma-separated list of formats. Default is to take extension of out or else gltf")
 @click.option('--check/--no-check', default=False, help="check produced file (currently only for usdz)")
-def export(data, out=None, format=None, check=False):
+def main(data, out=None, format=None, check=False):
     """Convert the DATA.json to OUT.format."""
     if out is '':
         out = None
@@ -43,33 +41,7 @@ def export(data, out=None, format=None, check=False):
     assert out is None or len(format_list) == 1
     assert all(_ in 'gltf glb usdz usda obj'.split() for _ in format_list)
     del format # will be set in for loop below
-
-    out_name_generated = False
-    for format in format_list:
-        if out is None or out_name_generated:
-            out_name_generated = True
-            out = data_path.with_suffix("."+format)
-        print(f"Generating format {format} in file {out}")
-        mode = 'wb' if format.startswith("usd") or format == 'glb' else 'w'
-        with open(out, mode) as outfile:
-            if format == 'gltf':
-                result = data2gltf(input).format_gltf()
-                json.dump(result, outfile)
-            elif format == 'glb':
-                result = data2gltf(input).format_glb()
-                outfile.write(result)
-            elif format == 'obj':
-                result = data2obj(input)
-                outfile.write(result)
-            elif format == 'usda':
-                usda, assets = data2usd_ascii(input)
-                outfile.write(usda.encode("utf-8"))
-                for key, val in assets.items():
-                    with open(key, 'wb') as f:
-                        f.write(val)
-            else:
-                result = data2usdz(input, save_usda=False, check=check)
-                outfile.write(result)
+    export(input, data_path, format_list, out, check)
 
 if __name__ == '__main__':
-    export()
+    main()
